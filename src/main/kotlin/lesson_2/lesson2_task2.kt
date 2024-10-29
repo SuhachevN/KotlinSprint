e84a3c91903fdf2120aaafcd46d1a93dbfc2c2be
SuhachevN <tradenoff@gmail.com>
2024-10-29 15:54:27 +0500

    KS-2-2 Calculated salary costs for permanent employees, total salary costs, average salary per employee after the placement of interns

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -5,9 +5,11 @@ fun main() {
     val employeePaycheck = 30000
     val numberOfInterns = 30
     val internPayCheck = 20000
+
     val expensesOnSalaries =  numberOfEmployees * employeePaycheck
     val totalExpenses = expensesOnSalaries + numberOfInterns * internPayCheck
     val averagePaycheck = totalExpenses / (numberOfEmployees + numberOfInterns)
+
     println("$expensesOnSalaries")
     println("$totalExpenses")
     println("$averagePaycheck")
