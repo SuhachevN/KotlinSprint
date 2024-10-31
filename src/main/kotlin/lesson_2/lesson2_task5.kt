f7d37faa297332592dec7fc675b544bfe825885c
SuhachevN <tradenoff@gmail.com>
2024-10-31 14:08:39 +0500

    KS-2-5 refactor interest rate inputs

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -4,10 +4,10 @@ import kotlin.math.pow
 
 fun main () {
     val principal = 70000
-    val rate = 16.7 / 100
+    val rate = 16.7
     val time = 20
 
-    val amount = principal * (1 + rate).pow(time)
+    val amount = principal * (1 + rate / 100).pow(time)
 
     println( "%.3f".format(amount))
 }
