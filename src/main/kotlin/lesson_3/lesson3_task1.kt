ff4dcf00ce9744ae805c8299799da15ee9f9be23
SuhachevN <tradenoff@gmail.com>
2024-11-06 15:47:20 +0500

    KS-3-1 create welcome message

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_3
+
+fun main() {
+    val userName = "Иван"
+    var greeting: String
+
+    greeting = "Доброе утро, $userName!"
+    println(greeting)
+
+    greeting = "Добрый вечер, $userName!"
+    println(greeting)
+}
