1fe30d8c866f34be4881fd248986e32881a6e22e
SuhachevN <tradenoff@gmail.com>
2024-11-12 14:48:40 +0500

    KS-3-2 Changing personal data in state app

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -1,17 +1,15 @@
 package org.example.lesson_3
 
 fun main() {
-    val nameAt20 = "Татьяна"
-    val surNameAt20 = "Андреева"
-    val patronymicAt20 = "Сергеевна"
-    val ageAt20 = 20
+    val name = "Татьяна"
+    var surName = "Андреева"
+    val patronymic = "Сергеевна"
+    var age = 20
 
-    println("$surNameAt20 $nameAt20 $patronymicAt20, возраст: $ageAt20 лет")
+    println("$surName $name $patronymic, $age лет")
 
-    val nameAt22 = "Татьяна"
-    val surNameAt22 = "Сидорова"
-    val patronymicAt22 = "Сергеевна"
-    val ageAt22 = 22
+    surName = "Сидорова"
+    age = 22
 
-    println("$surNameAt22 $nameAt22 $patronymicAt22, возраст: $ageAt22 года")
+    println("$name $surName $patronymic, $age года")
 }
