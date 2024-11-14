22b21932881cdfd71f70111e56889f95ecd67588
SuhachevN <tradenoff@gmail.com>
2024-11-14 20:18:04 +0500

    KS-3-4 create programm

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_3
+
+fun main() {
+    val from1 = "E2"
+    val to1 = "E4"
+    val moveNum1 = '1'
+
+    println("$from1-$to1; ход $moveNum1")
+
+    val from2 = "D2"
+    val to2 = "D4"
+    val moveNum2 = '2'
+
+    println("$from2-$to2; ход $moveNum2")
+
+}
