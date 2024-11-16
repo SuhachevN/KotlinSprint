02e2d42d3d5069d0c6763c7b7aab148fa72c393f
SuhachevN <tradenoff@gmail.com>
2024-11-16 22:52:39 +0500

    KS-4-2 created a program that  displays cargo compliance

---
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_4
+
+fun main() {
+    val cargoNum1Weight = 20
+    val cargoNum1Capacity = 80
+
+    val cargoNum2Weight = 50
+    val cargoNum2Capacity = 100
+
+    println("Груз с весом $cargoNum1Weight кг и объемом $cargoNum1Capacity л соответствует категории 'Average': ${cargoNum1Weight > MIN_WEIGHT && cargoNum1Weight <= MAX_WEIGHT && cargoNum1Capacity < MAX_CAPACITY}")
+    println("Груз с весом $cargoNum2Weight кг и объемом $cargoNum2Capacity л соответствует категории 'Average': ${cargoNum2Weight > MIN_WEIGHT && cargoNum2Weight <= MAX_WEIGHT && cargoNum2Capacity < MAX_CAPACITY}")
+}
+const val MIN_WEIGHT = 35
+const val MAX_WEIGHT = 100
+const val MAX_CAPACITY = 100
