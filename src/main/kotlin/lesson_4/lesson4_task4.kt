3eafa0c8e1447026b9cb395afd2535e30c87cc82
SuhachevN <tradenoff@gmail.com>
2024-12-07 21:07:33 +0500

    KS-4-4 created a program that notifies you of your current workouts

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -3,12 +3,12 @@ package org.example.lesson_4
 fun main() {
     val day = 5
     val isHandAndAbsDay = day % 2 != 0
-    val isLegsAndBackDay = day % 2 == 0
+
 
     println ("""
          Упражнения для рук:   $isHandAndAbsDay
-         Упражнения для ног:   $isLegsAndBackDay
-         Упражнения для спины: $isLegsAndBackDay
+         Упражнения для ног:   ${!isHandAndAbsDay}
+         Упражнения для спины: ${!isHandAndAbsDay}
          Упражнения для пресса:$isHandAndAbsDay
         """.trimIndent())
 }
