05438c10f77819fa45c1b04b5a57ad1548042499
SuhachevN <tradenoff@gmail.com>
2024-12-11 23:35:08 +0500

    KS-5-6 refactor IMT -> bmi and added new constanta CENTIMETERS_IN_METERS

--- src/main/kotlin/lesson_5/lesson5_task6.kt
+++ src/main/kotlin/lesson_5/lesson5_task6.kt
@@ -7,21 +7,22 @@ fun main() {
     println("Введите ваш рост в сантиметрах")
     val userGrowthCm = readln().toFloat()
 
-    val userGrowthM = userGrowthCm / 100
+    val userGrowthM = userGrowthCm / CENTIMETERS_IN_METERS
 
-    val IMT = userWeight / (userGrowthM * userGrowthM)
+    val bmi = userWeight / (userGrowthM * userGrowthM)
 
     val category = when {
-        IMT < 18.5 -> "Недостаточная масса тела"
-        IMT in 18.5..24.9 -> "Нормальная масса тела"
-        IMT in 25.0..29.9 -> "Избыточная масса тела"
-        IMT in 30.0..34.9 -> "ожирение I-й степени"
-        IMT in 35.0..39.9 -> "ожирение II-й степени"
-        IMT >= 40 -> "ожирение III-й степени, суперожирение"
+        bmi < 18.5 -> "Недостаточная масса тела"
+        bmi in 18.5..24.9 -> "Нормальная масса тела"
+        bmi in 25.0..29.9 -> "Избыточная масса тела"
+        bmi in 30.0..34.9 -> "ожирение I-й степени"
+        bmi in 35.0..39.9 -> "ожирение II-й степени"
+        bmi >= 40 -> "ожирение III-й степени, суперожирение"
 
-        else -> "Что сюда написать..."
+        else -> "неизвестная степень"
     }
 
-    println("Ваш ИМТ: %.2f".format(IMT))
+    println("Ваш ИМТ: %.2f".format(bmi))
     println("Категория: $category")
 }
+const val CENTIMETERS_IN_METERS = 100
