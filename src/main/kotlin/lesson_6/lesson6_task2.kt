9a4dae899fd996a721d45f2c7eea96461897b940
SuhachevN <tradenoff@gmail.com>
2024-12-19 01:13:22 +0500

    KS-6-2 refactor -> delete cycle while

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -1,20 +1,9 @@
 package org.example.lesson_6
 
 fun main() {
-    println("Введите количество секунд, которые надо засечь:")
+    println("Введите количество секунд для таймера:")
     val seconds = readln().toInt()
 
-    if (seconds > 0) {
-        var countedTime = 0
-
-        println("Таймер запущен на $seconds секунд(ы)...")
-
-        while (countedTime < seconds) {
-            Thread.sleep(1000)
-            println("${countedTime++}")
-        }
-        println("Прошло $seconds секунд")
-    } else {
-        println("Введите данные корректно")
-    }
+    Thread.sleep(seconds * 1000L)
+    println("Прошло $seconds секунд.")
 }
