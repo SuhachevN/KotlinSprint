377a17f36d75362281c7263055880f2381c339a8
SuhachevN <tradenoff@gmail.com>
2025-02-26 23:32:37 +0500

    refactor

--- src/main/kotlin/lesson_10/lesson10_task5.kt
+++ src/main/kotlin/lesson_10/lesson10_task5.kt
@@ -22,12 +22,15 @@ fun authorize(login: String, password: String): String? {
 
 fun generateToken(): String {
     val chars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
-    return (1..32).map { chars.random() }.joinToString("")
+    return (1..TOKEN_LENGTH).map { chars.random() }.joinToString("")
 }
 
 fun getCart(token: String): List<String>? {
-    return if (token.length == 32) CART_ITEMS else null
+    return if (token.length == TOKEN_LENGTH) getCartItems() else emptyList()
 }
-val CART_ITEMS = listOf("Товар A", "Товар B", "Товар C")
+fun getCartItems(): List<String> {
+    return listOf("Товар A", "Товар B", "Товар C")
+}
 const val VALID_LOGIN = "user123"
 const val VALID_PASSWORD = "password123"
+const val TOKEN_LENGTH = 32
