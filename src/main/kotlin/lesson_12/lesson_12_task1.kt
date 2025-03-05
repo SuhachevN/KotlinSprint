3c35d7ee0bd705d49879043c192e987ff385e79b
SuhachevN <tradenoff@gmail.com>
2025-03-05 21:03:23 +0500

    KS-12-1 refactor var -> val

--- src/main/kotlin/lesson_12/lesson_12_task1.kt
+++ src/main/kotlin/lesson_12/lesson_12_task1.kt
@@ -1,33 +1,28 @@
 package org.example.lesson_12
 
-class Weather() {
+class Weather {
+    val dayTemperature: Int
+    val nightTemperature: Int
+    val precipitation: Boolean
 
-    var dayTemperature: Int = 1
-    var nightTemperature: Int = 2
-    var precipitation: Boolean = false
+    constructor(dayTemperature: Int, nightTemperature: Int, precipitation: Boolean) {
+        this.dayTemperature = dayTemperature
+        this.nightTemperature = nightTemperature
+        this.precipitation = precipitation
+    }
 
     fun displayWeather() {
-        println("Дневная температура: $dayTemperature")
-        println("Ночная температура: $nightTemperature")
-        println("Наличие осадков: ${if (precipitation) "Да" else "Нет"}")
+        println("Дневная температура: $dayTemperature, Ночная температура: $nightTemperature, Наличие осадков: ${if (precipitation) "Да" else "Нет"}")
     }
 }
 
 fun main() {
-    val Weather1 = Weather()
-    val Weather2 = Weather()
-
-    Weather1.dayTemperature = 22
-    Weather1.nightTemperature = 17
-    Weather1.precipitation = false
-
-    Weather2.dayTemperature = 24
-    Weather2.nightTemperature = 16
-    Weather2.precipitation = true
+    val weather1 = Weather(dayTemperature = 22, nightTemperature = 17, precipitation = false)
+    val weather2 = Weather(dayTemperature = 24, nightTemperature = 16, precipitation = true)
 
     println("Погода первого дня:")
-    Weather1.displayWeather()
+    weather1.displayWeather()
 
     println("\nПогода второго дня:")
-    Weather2.displayWeather()
+    weather2.displayWeather()
 }
