4071d8d1e1398e2bed799d34c45880306b430f33
SuhachevN <tradenoff@gmail.com>
2025-03-06 00:39:37 +0500

    KS-14-1 create SuperClass - SuperShip, three subclasses - liner, icebreaker and cargo.

---
+++ src/main/kotlin/lesson_14/lesson14_task1.kt
@@ -0,0 +1,56 @@
+package org.example.lesson_14
+
+open class SuperShip(
+    val name: String,
+    val speed: Int,
+    val capacity: Int,
+    val iceBreak: Boolean = false,
+) {
+    open fun displayInfo() {
+        println("Корабль: $name, скорость: $speed миль в час, вместимость: $capacity")
+    }
+}
+
+class CargoShip(
+    name: String = "Грузовой корабль",
+    speed: Int = 40,
+    capacity: Int = 600,
+) : SuperShip(name, speed, capacity) {
+    override fun displayInfo() {
+        println("Грузовой Корабль: $name, Скорость: $speed узлов, Грузоподъемность: $capacity тонн")
+    }
+}
+
+class Liner(
+    name: String = "Лайнер",
+    speed: Int = 110,
+    capacity: Int = 1000,
+) : SuperShip(name, speed, capacity) {
+    override fun displayInfo() {
+        println("Лайнер: $name, Скорость: $speed миль в час, Вместимость: $capacity пассажиров")
+    }
+}
+
+class Icebreaker(
+    name: String = "Ледокол",
+    speed: Int = 30,
+    capacity: Int = 200,
+) : SuperShip(name, speed, capacity, iceBreak = true) {
+    override fun displayInfo() {
+        println("Ледокол: $name, Скорость: $speed миль в час, Вместимость: $capacity тонн")
+    }
+}
+
+
+fun main() {
+    val cargoShip = CargoShip(name = "Бисмарк")
+    val icebreaker = Icebreaker(name = "Наутилус-1")
+    val liner = Liner(name = "Аврора")
+
+    cargoShip.displayInfo()
+    println()
+    icebreaker.displayInfo()
+    println(icebreaker.iceBreak)
+    println()
+    liner.displayInfo()
+}
