407ccc3a2681396fab5290fef7d1896f97dcc3d8
SuhachevN <tradenoff@gmail.com>
2025-03-07 03:09:15 +0500

    KS-12-3 add new variable - kelvinToCelsiusConversion

--- src/main/kotlin/lesson_12/lesson12_task3.kt
+++ src/main/kotlin/lesson_12/lesson12_task3.kt
@@ -1,15 +1,16 @@
 package org.example.lesson_12
 
 class CurrentWeather(temperatureDayInKalvin: Int, temperatureNightInKalvin: Int, precip: Boolean,) {
-    val dayTemp: Int = temperatureDayInKalvin - 273
-    val nightTemp: Int = temperatureNightInKalvin - 273
-    val precipitation: Boolean = precip
+    val kelvinToCelsiusConversion = 273
+    val dayTemp: Int = temperatureDayInKalvin - kelvinToCelsiusConversion
+    val nightTemp: Int = temperatureNightInKalvin - kelvinToCelsiusConversion
+    val isPrecipitation: Boolean = precip
 
 
     fun displayWeather() {
         println("Дневная температура: $dayTemp°C")
         println("Ночная температура: $nightTemp°C")
-        println("Наличие осадков: ${if (precipitation) "Да" else "Нет"}")
+        println("Наличие осадков: ${if (isPrecipitation) "Да" else "Нет"}")
     }
 }
 
