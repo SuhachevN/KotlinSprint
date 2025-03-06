96067f3ba20cf6396ad3f62caa414661ef52f321
SuhachevN <tradenoff@gmail.com>
2025-03-07 03:04:19 +0500

    KS-12-2 refactor precipitation -> isPrecipitation

--- src/main/kotlin/lesson_12/lesson12_task2.kt
+++ src/main/kotlin/lesson_12/lesson12_task2.kt
@@ -3,15 +3,15 @@ package org.example.lesson_12
 class DailyWeather(
     var dayTemp: Int,
     var nightTemp: Int,
-    var precipitation: Boolean,
+    var isPrecipitation: Boolean,
 ){
     fun displayWeather() {
         println("Дневная температура: $dayTemp")
         println("Ночная температура: $nightTemp")
-        println("Наличие осадков: ${if (precipitation) "Да" else "Нет"}")
+        println("Наличие осадков: ${if (isPrecipitation) "Да" else "Нет"}")
     }
 }
 fun main() {
-    val todayWeather = DailyWeather(dayTemp = 21, nightTemp = 17, precipitation = true)
+    val todayWeather = DailyWeather(dayTemp = 21, nightTemp = 17, isPrecipitation = true)
     todayWeather.displayWeather()
 }
