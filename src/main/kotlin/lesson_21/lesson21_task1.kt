1ffeda9de42b7ec6e2c279bcca8032afbe8c3a5a
SuhachevN <tradenoff@gmail.com>
2025-03-28 00:22:16 +0500

    KS-21-1 string extension with vowel counting

---
+++ src/main/kotlin/lesson_21/lesson21_task1.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_21
+
+fun String.vowelExtension(): Int {
+    val vowels = "aeiouAEIOUуеыаояиюУЕЫАОЯИЮ"
+    return this.count { it in vowels }
+}
+
+fun main() {
+    val num1 = "hello"
+    val num2 = "Привет"
+    val num3 = "Сияет"
+    val num4 = "extension"
+
+    println("Количество гласных в '$num1': ${num1.vowelExtension()}")
+    println("Количество гласных в '$num2': ${num2.vowelExtension()}")
+    println("Количество гласных в '$num3': ${num3.vowelExtension()}")
+    println("Количество гласных в '$num4': ${num4.vowelExtension()}")
+
+}
